125d904d9437a1d25ee1572180604c5a9763e399
easifem <vickysharma0812@gmail.com>
2025-05-08 18:35:24 +0900

    updates in lsp.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -56,7 +56,7 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     vim.cmd(
       [[ set efm=%-Ggfortran%.%#,%A%f:%l:%c:,%A%f:%l:,%C,%C%p%*[0123456789^],%Z%trror:\ %m,,%Z%tarning:\ %m,%C%.%#,%-G%.%# ]]
-      -- [[ set efm=%A%f:%l:%c:,%C,%C,%C,%Z%trror:\ %m,,%Z%tarning:\ %m,%C%.%#,%-G%.%# ]]
+    -- [[ set efm=%A%f:%l:%c:,%C,%C,%C,%Z%trror:\ %m,,%Z%tarning:\ %m,%C%.%#,%-G%.%# ]]
     )
     vim.g.use_myfmt = false
   end,
@@ -96,9 +96,9 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     local wk = require("which-key")
     wk.add({
-      { "<leader>m", group = "+Markdown" },
-      { "<leader>mp", "<cmd>MarkdownPreview<CR>", desc = "markdown preview" },
-      { "<leader>ms", "<cmd>MarkdownPreviewStop<CR>", desc = "stop markdown preview" },
+      { "<leader>m",  group = "+Markdown" },
+      { "<leader>mp", "<cmd>MarkdownPreview<CR>",       desc = "markdown preview" },
+      { "<leader>ms", "<cmd>MarkdownPreviewStop<CR>",   desc = "stop markdown preview" },
       { "<leader>mr", "<cmd>RenderMarkdown toggle<CR>", desc = "toggle render markdown" },
     })
   end,
@@ -118,7 +118,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     local wk = require("which-key")
     wk.add({
-      { "<leader>T", group = "+Typst" },
+      { "<leader>T",  group = "+Typst" },
       { "<leader>Tw", "<cmd>TypstWatch<CR>", desc = "watch typst docment" },
     })
   end,
@@ -180,5 +180,65 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
     vim.cmd.setlocal("norelativenumber")
     vim.wo.signcolumn = "no"
     set_terminal_keymaps()
+  end,
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  group = augroup("pengvim_mathhovar"),
+  pattern = { "*" },
+  callback = function()
+    local ft = vim.bo.filetype
+    if ft == "markdown" or ft == "quarto" then
+      require("nabla").popup()
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("pengvim-lsp-attach", { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc)
+      vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+    end
+    local function vmap(keys, func, desc)
+      vim.keymap.set("v", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+    end
+
+    map("gh", vim.lsp.buf.signature_help, "[g]o to signature [h]elp")
+    map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+    map("K", vim.lsp.buf.hover, "Hover Documentation")
+    vmap("<leader>Lf", vim.lsp.buf.format, "[l]sp [f]ormat")
+
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    assert(client, "LSP client not found")
+
+    if client and client.server_capabilities.documentHighlightProvider then
+      local highlight_augroup = vim.api.nvim_create_augroup("pengvim-lsp-highlight", { clear = false })
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd("LspDetach", {
+        group = vim.api.nvim_create_augroup("pengvim-lsp-detach", { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds({ group = "pengvim-lsp-highlight", buffer = event2.buf })
+        end,
+      })
+    end
+
+    if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+      map("<leader>th", function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+      end, "[T]oggle Inlay [H]ints")
+    end
   end,
 })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,54 +38,6 @@ return
         },
       })
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("pengvim-lsp-attach", { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-          end
-          local function vmap(keys, func, desc)
-            vim.keymap.set("v", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-          end
-
-          map("gh", vim.lsp.buf.signature_help, "[g]o to signature [h]elp")
-          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-          map("K", vim.lsp.buf.hover, "Hover Documentation")
-          vmap("<leader>Lf", vim.lsp.buf.format, "[l]sp [f]ormat")
-
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          assert(client, "LSP client not found")
-
-          if client and client.server_capabilities.documentHighlightProvider then
-            local highlight_augroup = vim.api.nvim_create_augroup("pengvim-lsp-highlight", { clear = false })
-            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd("LspDetach", {
-              group = vim.api.nvim_create_augroup("pengvim-lsp-detach", { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds({ group = "pengvim-lsp-highlight", buffer = event2.buf })
-              end,
-            })
-          end
-
-          if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-            map("<leader>th", function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-            end, "[T]oggle Inlay [H]ints")
-          end
-        end,
-      })
 
       local lsp_flags = {
         allow_incremental_sync = true,
